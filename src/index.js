70857c3df645f05d278ed1f156ee90cdfa5a5b6d
Aniket Pawar <anipawar9028@gmai.com>
2026-06-25 13:56:43 +0530

    DB Create

---
+++ src/index.js
@@ -0,0 +1,37 @@
+// require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv";
+
+import mongoose, { connect } from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+
+
+connectDB();
+
+/*
+import express from "express";
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+    application.on("error", (error) => {
+      console.log("ERR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("ERROR: ", error);
+    throw err;
+  }
+})();
+
+*/
